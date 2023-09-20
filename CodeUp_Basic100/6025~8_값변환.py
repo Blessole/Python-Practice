7ddf83742a7a7d68f31913fb180a7dd48a144f5d
sol.kim <sol.kim@lgepartner.com>
2023-09-20 23:58:50 +0900

    230920

---
+++ CodeUp_Basic100/6025~8_값변환.py
@@ -0,0 +1,18 @@
+# 6025 정수 2개 입력받아 합 계산하기
+a, b = input().split()
+print(int(a) + int(b))
+
+# 6026 실수 2개 입력받아 합 계산하기
+a = input()
+b = input()
+print(float(a) + float(b))
+
+# 6027 10진 정수 입력받아 16진수로 출력하기1
+a = input()
+n = int(a)
+print('%x' % n)
+
+# 6028 10진 정수 입력받아 16진수로 출력하기2
+a = input()
+n = int(a)
+print('%X' % n)
